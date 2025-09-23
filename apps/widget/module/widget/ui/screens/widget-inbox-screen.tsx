a8b037d4a1235aae6a32bb53e9f3ca65792d498b
okekefrancis112 <okeke98@gmail.com>
2025-09-23 13:14:47 +0100

    widget inbox

---
+++ apps/widget/module/widget/ui/screens/widget-inbox-screen.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useAtomValue, useSetAtom } from "jotai";
+import { contactSessionIdAtomFamily, conversationIdAtom, errorMessageAtom, organizationIdAtom, screenAtom } from "../../atoms/widget-atoms";
+import { WidgetHeader } from "../components/widget-header";
+import { AlertTriangleIcon, ArrowLeftIcon } from "lucide-react";
+import { WidgetFooter } from "../components/widget-footer";
+import { Button } from "@workspace/ui/components/button";
+import { set } from "zod/v4-mini";
+import { usePaginatedQuery, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { ConversationStatusIcon } from "@workspace/ui/components/conversation-status-icon";
+import { useInfiniteScroll } from "@workspace/ui/hooks/use-infinite-scroll";
+import { InfiniteScrollTrigger } from "@workspace/ui/components/infinite-scroll-trigger";
+
+export const WidgetInboxScreen = () => {
+    const setScreen = useSetAtom(screenAtom);
+    const setConversationId = useSetAtom(conversationIdAtom);
+
+        const organisationId = useAtomValue(organizationIdAtom);
+        const conversationId = useAtomValue(conversationIdAtom);
+        const contactSessionId = useAtomValue(
+            contactSessionIdAtomFamily(organisationId || "")
+        );
+
+        const conversations = usePaginatedQuery(
+            api.public.conversations.getMany,
+            contactSessionId
+                ?   {
+                        contactSessionId,
+                    }
+                :   "skip",
+            {
+                initialNumItems: 10,
+            },
+        );
+
+    function formatDistanceToNow(arg0: Date): import("react").ReactNode {
+        throw new Error("Function not implemented.");
+    }
+
+    const { topElementRef, handleLoadMore, canLoadMore, isLoadingMore } = useInfiniteScroll({
+            status: conversations.status,
+            loadMore: conversations.loadMore,
+            loadSize: 10,
+        });
+
+    return (
+        <>
+            <WidgetHeader>
+                <Button
+                    variant="transparent"
+                    size="icon"
+                    onClick={() => setScreen("selection")}
+                >
+                    <ArrowLeftIcon />
+                </Button>
+                <p>Inbox</p>
+            </WidgetHeader>
+            <div className="flex flex-1 flex-col gap-y-2 p-4 overflow-y-auto">
+                {conversations?.results.length > 0 &&
+                conversations?.results.map((conversation) => (
+                    <Button
+                        className="h-20 w-full justify-between"
+                        key={conversation._id}
+                        onClick={() => {
+                            setConversationId(conversation._id);
+                            setScreen("chat");
+                        }}
+                        variant="outline"
+                    >
+                        <div className="flex w-full flex-col gap-4 overflow-hidden text-start">
+                            <div className="flex w-full items-center justify-between gap-x-2">
+                                <p className="text-muted-foreground text-xs">chat</p>
+                                <p className="text-muted-foreground text-xs">
+                                    {formatDistanceToNow(new Date(conversation._creationTime))}
+                                </p>
+                            </div>
+                            <div className="flex w-full items-center justify-between gap-x-2">
+                                <p className="truncate text-sm">
+                                    {conversation.lastMessage?.text}
+                                </p>
+                                <ConversationStatusIcon status={conversation.status} className="shrink-0" />
+                            </div>
+                        </div>
+                    </Button>
+                ))}
+                <InfiniteScrollTrigger
+                    ref={topElementRef}
+                    onLoadMore={handleLoadMore}
+                    canLoadMore={canLoadMore}
+                    isLoadingMore={isLoadingMore}
+                />
+                {/* TODO: TEST THIS SPECIFICALLY */}
+                {conversations?.results.length === 0 && (
+                    <div className="flex flex-1 flex-col items-center justify-center gap-2">
+                        <p className="text-sm text-muted-foreground">No conversations yet</p>
+                    </div>
+                )}
+            </div>
+            <WidgetFooter />
+        </>
+    );
+};
