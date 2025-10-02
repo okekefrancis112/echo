ea04e6ff06e1ba46158af58ef52dd344220cfa32
okekefrancis112 <okeke98@gmail.com>
2025-10-02 02:18:21 +0100

    search tool

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -6,6 +6,7 @@ import { paginationOptsValidator } from "convex/server";
 import { escalateConversation } from "../system/ai/tools/escalateConversation";
 import { resolveConversation } from "../system/ai/tools/resolveConversation";
 import { saveMessage } from "@convex-dev/agent";
+import { search } from "../system/ai/tools/search";
 
 export const create = action({
     args: {
@@ -56,8 +57,9 @@ export const create = action({
                 {
                     prompt: args.prompt,
                     tools: {
-                        resolveConversation,
-                        escalateConversation,
+                        resolveConversationTool: resolveConversation,
+                        escalateConversationTool: escalateConversation,
+                        searchTool: search,
                     },
                 },
             )
