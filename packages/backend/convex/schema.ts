4bee8926c79272196c286a1e1a593b8ec0ab5386
okekefrancis112 <okeke98@gmail.com>
2025-11-14 05:31:51 +0100

    subscriptions

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,8 +1,13 @@
-import { PhoneNumber } from "@clerk/backend";
+import { Organization, PhoneNumber } from "@clerk/backend";
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+    subscriptions: defineTable({
+        organizationId: v.string(),
+        status: v.string(),
+    })
+        .index("by_organization_id", ["organizationId"]),
     widgetSettings: defineTable({
         organizationId: v.string(),
         greetMessage: v.string(),
