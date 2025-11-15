21d4f263088e9d30713556d2f374630f3d672556
okekefrancis112 <okeke98@gmail.com>
2025-11-15 06:26:16 +0100

    fix: deployment issues

--- apps/web/modules/integrations/constants.ts
+++ apps/web/modules/integrations/constants.ts
@@ -24,7 +24,7 @@ export const INTEGRATIONS = [
 
 export type IntegrationId = (typeof INTEGRATIONS)[number]["id"];
 
-export const HTML_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const REACT_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const NEXTJS_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const JAVASCRIPT_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const HTML_SCRIPT = `<script src="https://echo-widget-three.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const REACT_SCRIPT = `<script src="https://echo-widget-three.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const NEXTJS_SCRIPT = `<script src="https://echo-widget-three.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const JAVASCRIPT_SCRIPT = `<script src="https://echo-widget-three.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
